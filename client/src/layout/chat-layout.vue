3ca46dd93ce1d320d7f9ea74643d44f984b904a9
YarMolenko13 <65036611+YarMolenko13@users.noreply.github.com>
2021-08-13 13:43:04 +0300

    ui fixes

---
+++ client/src/layout/chat-layout.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="main-layout chat-layout">
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "chat-layout"
+}
+</script>
+
+<style lang="scss">
+    .chat-layout {
+        min-height: auto;
+    }
+</style>
